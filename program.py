9424aced4a6bb709d33fe8d2e7f0f2fa092c605b
marimalll <123314557+marimalll@users.noreply.github.com>
2023-04-10 17:23:02 +0500

    site_commit

--- program.py
+++ program.py
@@ -1,6 +1,9 @@
 def main():
     print('My first git program')
     print('And I change it every day')
+    s = [1, 2, 3, 45, 6, 7, 8, 76,8]
+    s.sort()
+    print(s)
 
 
 if __name__ == '__main__':
